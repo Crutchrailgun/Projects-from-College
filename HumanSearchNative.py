7ffa012ee232e6655c1f5fec593801ca03816dcf
Crutchrailgun <jtfout0@gmail.com>
2026-09-08 13:20:55 -0400

    Refactor native string search function to improve pattern counting and run tracking logic

--- HumanSearchNative.py
+++ HumanSearchNative.py
@@ -12,13 +12,19 @@ def native_string_search(file, pattern):
 
     with open(file, 'r') as f:
         content = f.read()
-        if pattern in content:
-            count += content.count(pattern)
-            #number of runs
+
+        n = len(content)
+        m = len(pattern)
+
+        for i in range(n - m + 1):
+            if content[i:i + m] == pattern:
+                count += 1
             runs += 1
+
+
+        print(f"Number of runs: {runs}")      
             
-    print(f"The pattern '{pattern}' was found {count} times in the file '{file}'.")
-    print(f"Number of runs: {runs}")
+        
     return count
 
 
