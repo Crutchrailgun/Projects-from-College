14108b745e47e4deadf40a03c6b96197ee81afa9
Crutchrailgun <jtfout0@gmail.com>
2026-09-08 11:40:34 -0400

    Add native string search function to read file and count pattern occurrences

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+from matplotlib import text
+
+def native_string_search(file, pattern):
+
+    file = input("Enter the file name: ")
+    pattern = input("Enter the pattern to search for: ")
+    #number of instances of the pattern in the file
+    count = 0
+
+    with open(file, 'r') as f:
+        content = f.read()
+        if pattern in content:
+            count+= 1
+
+
+    return count
